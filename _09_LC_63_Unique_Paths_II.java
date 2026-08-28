8cb627224a73f46a17d5ef097862aaeed907b16c
Dhruv Vishwakarma <dhruvavishw@gmail.com>
2026-08-28 21:59:02 +0530

    This is also by Tabulation

---
+++ _09_LC_63_Unique_Paths_II.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class _09_LC_63_Unique_Paths_II {
+
+    static int paths(int grid [][]){
+        int m=grid.length;
+        int n=grid[0].length;
+        int dp [][]=new int [m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else if(i==0) dp[i][j]=dp[i][j-1];
+                else if(j==0) dp[i][j]=dp[i-1][j];
+                else dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
+
+    }
+
+    public static void main(String BKP []){
+     int grid [] []={{0,0,0},{0,1,0},{0,0,0}};
+     System.out.println(paths(grid));
+    }
+}
