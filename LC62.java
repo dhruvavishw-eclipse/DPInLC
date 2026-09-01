08b8608be958c38493a24f7e762a546da7a6f5ce
Dhruv Vishwakarma <dhruvavishw@gmail.com>
2026-09-01 17:30:26 +0530

    Space Optimization is Here

--- LC62.java
+++ LC62.java
@@ -1,25 +1,46 @@
 import java.util.*;
 public class LC62 {
 
-
     public static int uniquePaths(int m, int n){
-    int dp [][]=new int [m][n];
-    for(int i=0;i<m;i++){
+        // SPACE OPTIMIZATION
+        int dp [][]=new int [2][n];
         for(int j=0;j<n;j++){
-            if(i==0 || j==0) dp[i][j]=1;
-            else dp[i][j]=dp[i][j-1]+ dp[i-1][j];
+            dp[0][j]=1;
+            dp[1][j]=1;
         }
+
+        for(int i=1;i<m;i++){
+
+            for(int j=1;j<n;j++){
+                dp[1][j]=dp[1][j-1]+dp[0][j];
+            }
+
+            for(int j=1;j<n;j++){
+                dp[0][j]=dp[1][j];
+            }
+        }
+        return dp[1][n-1];
     }
-    return dp[m-1][n-1];
 
 
-   }
+
+
+//     public static int uniquePaths(int m, int n){
+//     int dp [][]=new int [m][n];
+//     for(int i=0;i<m;i++){
+//         for(int j=0;j<n;j++){
+//             if(i==0 || j==0) dp[i][j]=1;
+//             else dp[i][j]=dp[i][j-1]+ dp[i-1][j];
+//         }
+//     }
+//     return dp[m-1][n-1];
+//    }
 
 
 
 
     //  public static int paths(int i,int j,int m,int n,int dp [][]){
-    //     // Recursive DP
+    //     // RECURSIVE DP
     //   if(i>=m || j>=n) return 0;
     //   if(i==m-1 && j==n-1) return 1;
     //  if(dp[i][j]!=-1) return dp[i][j];
